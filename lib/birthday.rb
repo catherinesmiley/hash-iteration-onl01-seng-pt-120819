68500a8834e530feeb049425557b62c65621aead
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 22:16:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,39 @@
 #   "Amanda" => 27
 # }
 
+# def happy_birthday(birthday_kids)
+#   birthday_kids.each do |kids_name, age|
+#     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+#   end
+# end
+
+# def age_appropriate_birthday(birthday_kids)
+#   birthday_kids.each do |kids_name, age|
+#     if age <= 12
+#       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+#     else
+#       puts "You are too old for this."
+#     end
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+# birthday_kids = {
+#   "Timmy" => 9, 
+#   "Sarah" => 6, 
+#   "Amanda" => 27
+# }
+
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
@@ -12,10 +45,12 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age <= 12
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else
+    if age > 12
       puts "You are too old for this."
+    else
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
 end
+
+
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,14 +6,41 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+# def select_winner(passengers)
+#   winner = ""
+#   passengers.each do |suite, name|
+#     if suite == :suite_a && name.start_with?("A")
+#       winner = name
+#     end
+#   end
+
+# winner
+
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
+      winner = name 
     end
   end
 
 winner
-
 end
